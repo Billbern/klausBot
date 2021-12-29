e6e1510f80d45beaaae22acc474af3c4e42ee461
Billbern <denis.abaidoo@gmail.com>
2021-12-29 15:35:53 +0000

    added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+# klausBot
--- main.py
+++ main.py
@@ -10,7 +10,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 @client.event
-async def on_ready():
+async def on_member_join(member):
+    await member.create_dm() 
     pass  
 
 
