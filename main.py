b42f03cc55155bb97e9c5ae5e43558206837f7a7
Billbern <denis.abaidoo@gmail.com>
2021-12-30 05:40:19 +0000

    test for messages

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_NAME')
 
 
 client = discord.Client()
 
 @client.event
 async def on_member_join(member):
-    print(f"{member} joined") 
+    await member.create_dm()
+    await member.dm_channel.send(f"Hello {member.name}, welcome to Programmer's Hub GH\nPlease introduce yourself " )
 
+@client.event
+async def on_message(message):
+    if message.startswith(";$"):
+        if message.split("$")[1] == "count":
+            print(message)
+            # message.channel.send()
+    pass
+    
 
 client.run(TOKEN)
