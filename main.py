a6bd29f5578068dc0a9f0b5d7e09818ee6bcbf2c
Billbern <denis.abaidoo@gmail.com>
2022-01-01 07:31:22 +0000

    fixed member count function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,11 +14,10 @@ GUILD = os.getenv('GUILD_NAME')
 
 client = discord.Client()
 
-
-def get_member_count():
-    for guild in client.get_all_members():
-        print(guild)
-
+def get_members_count(gld):
+    for guild in client.guilds:
+        if guild.name == gld:
+            return guild.member_count
 
 @client.event
 async def on_member_join(member):
@@ -31,8 +31,11 @@ async def on_member_join(member):
 async def on_message(message):
     if message.content.startswith(";$"):
         if message.content.split("$")[1] == "count":
-            memcount = get_member_count()
-            await message.channel.send("counted")
+            memcount = get_members_count(message.guild.name)
+            if memcount == 1:
+                await message.channel.send(f"There is only {memcount} member")
+            else:
+                await message.channel.send(f"There are only {memcount} members so far")
         if message.content.split("$")[1] == "Hi" or message.content.split("$")[1] == "Hello":
             await message.channel.send(f"@{message.author} Hello how are you")
     
